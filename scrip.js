3b0c998a9444a32c1b00f3022cc7e14457c35c47
Arkant04 <delacorte19@gmail.com>
2023-11-30 12:28:41 +0100

    MOD: se prueba el debugging

---
+++ scrip.js
@@ -0,0 +1,16 @@
+// function update(){
+//     const data = document.querySelector("#range").value
+//     console.log(data)
+//     document.querySelector("#progressBar").value = data
+// }
+
+let a = 100
+let b= 0
+for( let i = 0; i < 5; i++){
+    
+    b+=i
+}
+
+console.log(b)
+a++
+console.log(a)
