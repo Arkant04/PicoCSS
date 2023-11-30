718641c3938d03a652746d0f8bce604b6dd31342
Arkant04 <delacorte19@gmail.com>
2023-11-30 12:56:35 +0100

    MOD: Se añade contador

--- scrip.js
+++ scrip.js
@@ -1,16 +1,27 @@
-// function update(){
-//     const data = document.querySelector("#range").value
-//     console.log(data)
-//     document.querySelector("#progressBar").value = data
-// }
-
-let a = 100
-let b= 0
-for( let i = 0; i < 5; i++){
-    
-    b+=i
+function update(){
+    const data = document.querySelector("#range").value
+    console.log(data)
+    document.querySelector("#progressBar").value = data
 }
 
-console.log(b)
-a++
-console.log(a)
+// let a = 100
+// let b= 0
+// for( let i = 0; i < 5; i++){
+    
+//     b+=i
+// }
+
+// console.log(b)
+// a++
+// console.log(a)
+
+let contador = 0 
+function sumar(){
+   contador++
+   document.querySelector("#contador").innerHTML = contador
+}
+
+function restar(){
+    contador--
+    document.querySelector("#contador").innerHTML = contador
+ }
